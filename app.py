a4406efbf7ec47f8a44cf9dee83a452dbb0a415f
shivangraikar <shivangraikar@gmail.com>
2023-11-29 17:49:01 -0500

    integrating database with flask app

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import sqlite3
 
 app = Flask(__name__)
+app.config['DATABASE'] = 'forms.db'
 
-# SQLite database setup
-conn = sqlite3.connect('forms.db')
-cursor = conn.cursor()
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS forms (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        company TEXT NOT NULL,
-        invoice TEXT NOT NULL,
-        job TEXT NOT NULL
-    )
-''')
-conn.commit()
-conn.close()
+# Function to get a database connection
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db.row_factory = sqlite3.Row
+    return db
 
+# Function to close the database connection
+def close_db(e=None):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# Flask route for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Flask route to handle form submission
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
@@ -29,11 +32,9 @@ def submit():
         job = request.form['job']
 
         # Store data in the database
-        conn = sqlite3.connect('forms.db')
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO forms (company, invoice, job) VALUES (?, ?, ?)', (company, invoice, job))
-        conn.commit()
-        conn.close()
+        db = get_db()
+        db.execute('INSERT INTO forms (company, invoice, job) VALUES (?, ?, ?)', (company, invoice, job))
+        db.commit()
 
         return "Form submitted successfully!"
 
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS forms (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT NOT NULL,
+    invoice TEXT NOT NULL,
+    job TEXT NOT NULL
+);
