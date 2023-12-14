2de7a6f5550816b83f1f043ea509928996778152
shivangraikar <shivangraikar@gmail.com>
2023-12-14 15:45:05 -0500

    added pdf file column to schema and table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
+-- schema.sql
 CREATE TABLE IF NOT EXISTS forms (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     company TEXT NOT NULL,
     invoice TEXT NOT NULL,
-    job TEXT NOT NULL
+    job TEXT NOT NULL,
+    pdf_file BLOB NOT NULL
 );
